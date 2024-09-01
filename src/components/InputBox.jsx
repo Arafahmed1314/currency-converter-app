4de8937b4037703bb3d939a99bda0b77717ab52a
Naimul Islam <nayemhasan1314@gmail.com>
2024-09-01 15:55:26 +0600

    some changes

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -6,15 +6,15 @@ function InputBox({
     onAmountChange,
     onCurrencyChange,
     currencyOptions = [],
-    selectCurrency = "usd",
+    selectCurrency = "USD",
     amountDisable = false,
     currencyDisable = false,
-    className = "",
+
 }) {
    const amountInputId = useId()
 
     return (
-        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+        <div className="bg-white p-3 rounded-lg text-sm flex">
             <div className="w-1/2">
                 <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block">
                     {label}
@@ -40,7 +40,7 @@ function InputBox({
 
                         {currencyOptions.map((currency) => (
                             <option key={currency} value={currency}>
-                            {currency}
+                            {currency.toUpperCase()}
                             </option>
                         ))}
 
